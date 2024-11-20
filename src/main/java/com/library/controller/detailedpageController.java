42a15e3b54100ae649b6dacefaec24aa46ad8f60
cuase12 <bakym608@naver.com>
2024-11-21 00:43:02 +0900

    mappig추가

--- src/main/java/com/library/controller/detailedpageController.java
+++ src/main/java/com/library/controller/detailedpageController.java
@@ -1,4 +1,12 @@
 package com.library.controller;
 
-public class detailedpage {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class detailedpageController {
+    @GetMapping("/bookdetails")
+    public String bookdetails() {
+        return "bookdetails";
+    }
 }
--- src/main/java/com/library/controller/wishbookController.java
+++ src/main/java/com/library/controller/wishbookController.java
@@ -1,4 +1,11 @@
 package com.library.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class wishbookController {
+    @GetMapping("/wishbookstatus")
+    public String wishbookstatus() {
+        return "wishbookstatus";
+    }
 }
