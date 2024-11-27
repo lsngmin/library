c77ebd46ab6903b011170b26d771fd4cf1c16653
lsngmin <lsngmin@naver.com>
2024-11-27 22:37:23 +0900

    deploy config

--- src/main/java/com/library/main.java
+++ src/main/java/com/library/main.java
@@ -1,11 +1,20 @@
 package com.library;
 
+
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class main {
-    public static void main(String[] args) {
-        // ApplicationContext를 초기화 (Bean 설정 XML 파일 로드)
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        tomcat.addWebapp("/", System.getProperty("user.dir") + "/src/main/webapp");
+        tomcat.start();
+        tomcat.getServer().await(); // Tomcat이 종료될 때까지 대기
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         System.out.println("Spring Framework Application Started!");
